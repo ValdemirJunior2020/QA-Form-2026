746806a30075428a6aedc66e490b03129ef2d7f1
Valdemir Junior <infojr.8@gmail.com>
2026-05-25 15:35:41 -0400

    jgbkugb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1114,7 +1114,7 @@ function FunnyLoadingScreen({ title, error = "", onRetry }) {
   useEffect(() => {
     const interval = window.setInterval(() => {
       setMessageIndex((prev) => (prev + 1) % FUNNY_QA_LOADING_MESSAGES.length);
-    }, 1000);
+    }, 2500);
 
     return () => window.clearInterval(interval);
   }, []);
