2d25751352f282c8de52fdabc2af716a8b709e1d
ValdemirJunior2020 <your-infojr.83@gmail.com>
2026-05-27 12:03:23 -0400

    kojihubvb

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,17 @@
-// /client/src/main.jsx
+// C:\Users\Valdemir Goncalves\Desktop\Meus Projetos\qa-form-react-project\client\src\main.jsx
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element #root was not found.");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
